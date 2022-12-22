0326102b487a73a9a6403563919bd193aaea077f
Muhamad Zaki Raihan <toteyoku4636@gmail.com>
2022-12-22 16:40:48 +0700

    add task api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/task', [TaskController::class, 'index']);
+
+Route::post('/task', [TaskController::class, 'store']);
+
+Route::put('/task/{id}', [TaskController::class, 'update']);
+
+Route::delete('/task/{id}', [TaskController::class, 'destroy']);
