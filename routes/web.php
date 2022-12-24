1df9c2bd83d03cbea42376853c2e252358bf2432
annisam23 <annisamrahma23@gmail.com>
2022-12-24 20:46:25 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\TaskController;
 
 
